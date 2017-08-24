64f265d9b64dad7028d278c77770663fe4b6f164
Unknown <kurnianggoro@gmail.com>
2017-08-24 17:41:38 +0900

    update the documentation

--- modules/face/include/opencv2/face/facemark.hpp
+++ modules/face/include/opencv2/face/facemark.hpp
@@ -199,24 +199,32 @@ namespace face {
         // virtual void read( const FileNode& fn )=0;
         // virtual void write( FileStorage& fs ) const=0;
 
-        virtual void addTrainingSample(InputArray image, InputArray landmarks)=0;
+        /** @brief Add one training sample to the trainer.
 
-        /** @brief Trains a Facemark algorithm using the given dataset.
-
-        @param imageList This file contains the list of image names in the dataset.
-        @param groundTruth This file contains the list of ground-truth files in the dataset.
-        The contents in ground-truth files should follows the default formatting (see face::loadFacePoints).
+        @param image Input image.
+        @param landmarks The ground-truth of facial landmarks points corresponds to the image.
 
         <B>Example of usage</B>
         @code
-        FacemarkLBF::Params params;
-        params.model_filename = "ibug68.model"; // filename to save the trained model
-        Ptr<Facemark> facemark = FacemarkLBF::create(params);
         String imageFiles = "../data/images_train.txt";
         String ptsFiles = "../data/points_train.txt";
-        facemark->training(imageFiles, ptsFiles);
+        std::vector<String> images_train;
+        std::vector<String> landmarks_train;
+
+        // load the list of dataset: image paths and landmark file paths
+        loadDatasetList(imageFiles,ptsFiles,images_train,landmarks_train);
+
+        Mat image;
+        std::vector<Point2f> facial_points;
+        for(size_t i=0;i<images_train.size();i++){
+            image = imread(images_train[i].c_str());
+            loadFacePoints(landmarks_train[i],facial_points);
+            facemark->addTrainingSample(image, facial_points);
+        }
         @endcode
 
+        The contents in the training files should follows the standard format.
+        Here are examples for the contents in these files.
         example of content in the images_train.txt
         @code
         /home/user/ibug/image_003_1.jpg
@@ -232,7 +240,26 @@ namespace face {
         /home/user/ibug/image_005_1.pts
         /home/user/ibug/image_006.pts
         @endcode
+
         */
+        virtual void addTrainingSample(InputArray image, InputArray landmarks)=0;
+
+        /** @brief Trains a Facemark algorithm using the given dataset.
+        Before the training process, training samples should be added to the trainer
+        using face::addTrainingSample function.
+
+        <B>Example of usage</B>
+        @code
+        FacemarkLBF::Params params;
+        params.model_filename = "ibug68.model"; // filename to save the trained model
+        Ptr<Facemark> facemark = FacemarkLBF::create(params);
+
+        // add training samples (see Facemark::addTrainingSample)
+
+        facemark->training();
+        @endcode
+        */
+        
         virtual void training()=0;
 
         /** @brief A function to load the trained model before the fitting process.
